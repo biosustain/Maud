8dd46c1374f79fa4c111c5eadd23a7e1b16a90cd
Teddy Groves <tedgro@nnfcb-l0601.eduroam.wireless.dtu.dk>
2019-04-04 15:37:54 +0200

    feat: function for running a cmdstan model from python

--- python/stan_utils.py
+++ python/stan_utils.py
@@ -23,3 +23,20 @@ def StanModel_cache(model_input, model_name=None, **kwargs):
     else:
         print("Using cached StanModel")
     return sm
+
+
+def run_cmdstan_model(cmdstan_directory,
+                      path_from_cmdstan_directory_to_program,
+                      cmdstan_input_data_file,
+                      cmdstan_output_data_file):
+
+    here = os.path.dirname(__file__)
+    os.environ["CMDSTAN_DIRECTORY"] = cmdstan_directory
+    os.environ["PATH_FROM_CMDSTAN_DIRECTORY_TO_PROGRAM"] = path_from_cmdstan_directory_to_program
+    os.environ["CMDSTAN_INPUT_DATA_FILE"] = cmdstan_input_data_file
+    os.environ["CMDSTAN_OUTPUT_DATA_FILE"] = cmdstan_output_data_file
+
+    if not os.path.isfile(os.path.join(cmdstan_directory, path_from_cmdstan_directory_to_program)):
+        os.system("bash " + os.path.join(here, "../bash/compile_timecourse_model.sh"))
+
+    os.system("bash " + os.path.join(here, "../bash/run_timecourse_model.sh"))
