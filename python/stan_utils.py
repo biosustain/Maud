60787d6568f0dc64a0fe7c9e118a3e2f1725b31b
Teddy Groves <tedgro@nnfcb-l0601.eduroam.wireless.dtu.dk>
2019-04-15 10:16:14 +0200

    refactor: update stan utils to use cmdstan not pystan

--- python/stan_utils.py
+++ python/stan_utils.py
@@ -1,60 +1,37 @@
 import os
-import pystan
-import pickle
-from hashlib import md5
+
+PATH_FROM_HERE_TO_CMDSTAN_HOME = '../cmdstan'
 
 
-def StanModel_cache(model_input, model_name=None, **kwargs):
-    here = os.path.dirname(__file__)
-    with open(model_input, 'r') as f:
-        model_code = f.read()
-        f.close()
-    code_hash = md5(model_code.encode('ascii')).hexdigest()
-    if model_name is None:
-        cache_fn = os.path.join(here, '../stan/cached_stan_models/{}.pkl'.format(code_hash))
-    else:
-        cache_fn = os.path.join(here, '../stan/cached_stan_models/{}-{}.pkl'.format(model_name, code_hash))
-    try:
-        sm = pickle.load(open(cache_fn, 'rb'))
-    except:
-        sm = pystan.StanModel(model_code=model_code)
-        with open(cache_fn, 'wb') as f:
-            pickle.dump(sm, f)
-    else:
-        print("Using cached StanModel")
-    return sm
+def compile_stan_model_with_cmdstan(path_from_cmdstan_home_to_program):
+    here = os.path.dirname(os.path.abspath(__file__))
+    cmdstan_home = os.path.join(here, PATH_FROM_HERE_TO_CMDSTAN_HOME)
+    command = f"cd {cmdstan_home} && make {path_from_cmdstan_home_to_program}"
+    os.system(command)
 
 
-def StanModel_cache_code(model_code, model_name=None, **kwargs):
-    here = os.path.dirname(__file__)
-    code_hash = md5(model_code.encode('ascii')).hexdigest()
-    if model_name is None:
-        cache_fn = os.path.join(here, '../stan/cached_stan_models/{}.pkl'.format(code_hash))
-    else:
-        cache_fn = os.path.join(here, '../stan/cached_stan_models/{}-{}.pkl'.format(model_name, code_hash))
-    try:
-        sm = pickle.load(open(cache_fn, 'rb'))
-    except:
-        sm = pystan.StanModel(model_code=model_code)
-        with open(cache_fn, 'wb') as f:
-            pickle.dump(sm, f)
-    else:
-        print("Using cached StanModel")
-    return sm
+def run_compiled_cmdstan_model(program_path,
+                               input_path,
+                               output_path,
+                               method_config='sample',
+                               init_config = '',
+                               random_config='',
+                               refresh_config=''):
+    here = os.path.dirname(os.path.abspath(__file__))
+    
+    program_directory, program = os.path.split(program_path)
+    print(program)
 
+    command = f"""
+    cd {program_directory}
 
-def run_cmdstan_model(cmdstan_directory,
-                      path_from_cmdstan_directory_to_program,
-                      cmdstan_input_data_file,
-                      cmdstan_output_data_file):
-
-    here = os.path.dirname(__file__)
-    os.environ["CMDSTAN_DIRECTORY"] = cmdstan_directory
-    os.environ["PATH_FROM_CMDSTAN_DIRECTORY_TO_PROGRAM"] = path_from_cmdstan_directory_to_program
-    os.environ["CMDSTAN_INPUT_DATA_FILE"] = cmdstan_input_data_file
-    os.environ["CMDSTAN_OUTPUT_DATA_FILE"] = cmdstan_output_data_file
-
-    if not os.path.isfile(os.path.join(cmdstan_directory, path_from_cmdstan_directory_to_program)):
-        os.system("bash " + os.path.join(here, "../bash/compile_timecourse_model.sh"))
-
-    os.system("bash " + os.path.join(here, "../bash/run_timecourse_model.sh"))
+    ./{program} \
+    {method_config} \
+    data file={input_path} \
+    {init_config} \
+    {random_config} \
+    output file={output_path} \
+    {refresh_config}
+    """
+    print(command)
+    os.system(command)
