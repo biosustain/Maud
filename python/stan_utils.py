d9b40a8c15524d3a3babfcee9235d3afd103a6ae
Teddy Groves <tedgro@nnfcb-l0601.clients.net.dtu.dk>
2019-05-02 16:32:53 +0200

    Feat: run cmdstan programs in parallel for multiple chains

--- python/stan_utils.py
+++ python/stan_utils.py
@@ -1,4 +1,6 @@
 import os
+from multiprocessing import Pool
+
 
 PATH_FROM_HERE_TO_CMDSTAN_HOME = '../cmdstan'
 
@@ -16,22 +18,56 @@ def run_compiled_cmdstan_model(program_path,
                                method_config='sample',
                                init_config = '',
                                random_config='',
-                               refresh_config=''):
+                               refresh_config='',
+                               chains=1):
+    """Get a compiled cmdstan program to produce a csv of samples
+    
+    See the cmdstan manual for more about the commands and how to customize
+    them.
+    
+    The program will write a csv to `output_path` that can be read using
+    arviz's `from_pystan` function.
+
+    :param program_path: path to the compiled program :param input_path: path
+    to a stan Rdump file of input data 
+
+    :param output_path: path to the output csv 
+
+    :param method_config: customize the command at the method level 
+
+    :param init_config: optionally give the model a path to an Rdump of initial
+    parameter values 
+
+    :param random_config: optionally fix the random seed
+
+    :param refresh_config: set the refresh interval for progress reporting
+
+    :param chains: how many parallel chains do you want to run (probably no
+    more than there are cores on your computer)
+    
+    :return: None
+
+    """
     here = os.path.dirname(os.path.abspath(__file__))
     
     program_directory, program = os.path.split(program_path)
-    print(program)
+    path_template = output_path.replace('.csv', '{prefix}.csv')
+    command_template = f"""
+        cd {program_directory}
 
-    command = f"""
-    cd {program_directory}
+        ./{program} \
+        {method_config} \
+        data file={input_path} \
+        {init_config} \
+        {random_config} \
+        output file={path_template} \
+        {refresh_config}
+        """
+    commands = [command_template.format(prefix=str(c)) for c in range(chains)]
 
-    ./{program} \
-    {method_config} \
-    data file={input_path} \
-    {init_config} \
-    {random_config} \
-    output file={output_path} \
-    {refresh_config}
-    """
-    print(command)
-    os.system(command)
+    pool = Pool(processes=chains)
+    pool.map(os.system, commands)
+    replace = path_template.format(prefix='*')
+    cat_command = f"cat {replace} > {output_path}"
+    os.system(cat_command)
+    
