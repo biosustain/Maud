1a6158760db1492871fa7c5cfd9aa9b7211ad51c
Teddy Groves <tedgro@nnfcb-l0601.clients.net.dtu.dk>
2019-05-08 15:31:54 +0200

    Feat: put all model input data in one toml file

---
+++ enzymekat/python_modules/enzymekat_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import toml
+
+class EnzymeKatData():
+    """Object with all the data that is needed to fit an enzymeKat model, namely:
+
+      - Information about the experimental setup (measurement accuracy, time to
+        steady state)
+      - Information about kinetic parameters
+      - Information about ode metabolites
+
+    """
+    def __init__(self,
+                 experiment_info: pd.Series,
+                 ode_metabolites: pd.Series,
+                 known_reals: pd.Series,
+                 kinetic_parameters: pd.DataFrame):
+        self.experiment_info = experiment_info
+        self.ode_metabolites = ode_metabolites
+        self.known_reals = known_reals
+        self.kinetic_parameters = kinetic_parameters
+
+
+def from_toml(path):
+    t = toml.load(path)
+    return EnzymeKatData(
+        experiment_info=pd.Series(t['experiment_info']),
+        ode_metabolites=pd.DataFrame.from_records(t['ode_metabolites']),
+        known_reals=pd.Series(t['known_reals']),
+        kinetic_parameters=pd.DataFrame.from_records(t['kinetic_parameters'])
+    )
