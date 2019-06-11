be13b05b0402a9d066172bd9c47519a5c31cda58
NicholasCowie <nicow@biosustain.dtu.dk>
2019-06-11 14:09:50 +0200

    Feat: normal sd to lognormal sd

--- enzymekat/python_modules/conversion.py
+++ enzymekat/python_modules/conversion.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 def sem_pct_to_lognormal_sigma(sem_pct, mean, n=3):
-    sem = sem_pct/100 * mean
+    sem = sem_pct / 100 * mean
     s = sem * np.sqrt(n)
-    return (np.sqrt(np.log(1 + (s ** 2)/(mean ** 2))))
-    
+    return (np.sqrt(np.log(1 + (s ** 2) / (mean ** 2))))
+
+
+def norm_sd_to_lognormal_sd(s, mean):
+    return (np.sqrt(np.log(1 + (s ** 2) / (mean ** 2))))
