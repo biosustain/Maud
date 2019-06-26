b202e2763a64f2706faf871262298904ee6542c9
Teddy Groves <tedgro@nnfcb-l0601.clients.net.dtu.dk>
2019-06-26 17:26:17 +0200

    Bug: kwargs bug

--- enzymekat/python_modules/stan_utils.py
+++ enzymekat/python_modules/stan_utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def one_index_factorize(values, **kwargs):
-    labels, uniques = pd.factorize(values)
+    labels, uniques = pd.factorize(values, **kwargs)
     labels += 1
     return labels, uniques
