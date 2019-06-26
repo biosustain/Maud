1a7e3a2696858f0be37679b286b2208fbd7035b6
Teddy Groves <tedgro@nnfcb-l0601.clients.net.dtu.dk>
2019-06-26 17:28:52 +0200

    Feat: update data model

--- enzymekat/python_modules/enzymekat_data.py
+++ enzymekat/python_modules/enzymekat_data.py
@@ -26,14 +26,14 @@ class EnzymeKatData():
         self.constants = constants
         self.experiments = experiments
         self.reactions = reactions
-        self.stoichiometry = self.get_stoichoimetry()
-        self.measurements = self.get_measurements()
+        self.stoichiometry = self.get_stoichiometry()
+        self.flux_measurements, self.concentration_measurements = self.get_measurements()
         self.known_reals = self.get_known_reals()
-        self.parameters = self.get_parameters()
-        self.metabolites = self.get_metabolites()
+        self.kinetic_parameters, self.thermodynamic_parameters = self.get_parameters()
+        self.stan_codes = self.get_stan_codes()
 
-    def get_stoichoimetry(self):
-        out = (
+    def get_stoichiometry(self):
+        S = (
             pd.DataFrame.from_records(self.reactions)
             .set_index('name')
             ['stoichiometry']
@@ -41,23 +41,52 @@ class EnzymeKatData():
             .fillna(0)
             .astype(int)
         )
-        out.index.name = 'reaction'
-        out.columns.name = 'metabolite'
-        return out
+        S.index.name = 'reaction'
+        S.columns.name = 'metabolite'
+        return S
 
     def get_measurements(self):
-        out = pd.io.json.json_normalize(
+        stan_codes = self.get_stan_codes()
+        measurements = pd.io.json.json_normalize(
             self.experiments,
             'measurements',
             meta=['label'],
             meta_prefix='experiment_'
-        ).rename(columns={'experiment_label': 'experiment'})
-        out['metabolite'] = out['label'].where(out['type'] == 'metabolite')
-        out['reaction'] = out['label'].where(out['type'] == 'flux')
-        out['metabolite_code'], _ = one_index_factorize(out['metabolite'])
-        out['reaction_code'], _ = one_index_factorize(out['reaction'])
-        out['experiment_code'], _ = one_index_factorize(out['experiment'])
-        return out
+        )
+        measurements['reaction_code'] = measurements['label'].map(stan_codes['reaction'].get)
+        measurements['metabolite_code'] = measurements['label'].map(stan_codes['metabolite'].get)
+        measurements['experiment_code'] = measurements['experiment_label'].map(stan_codes['experiment'].get)
+        flux_measurements, conc_measurements = (
+            measurements
+            .query(f"type == '{t}'")
+            .dropna(how='all', axis=1)
+            .copy()
+            .rename(columns={'label': t + '_label'})
+            for t in ['flux', 'metabolite']
+        )
+        flux_measurements['reaction_code'] = flux_measurements['reaction_code'].astype(int)
+        conc_measurements['metabolite_code'] = conc_measurements['metabolite_code'].astype(int)
+        return flux_measurements, conc_measurements
+
+    def get_stan_codes(self):
+        S = self.get_stoichiometry()
+        known_reals = self.get_known_reals()
+        kinetic_parameters, thermodynamic_parameters = self.get_parameters()
+        experiment_labels = [e['label'] for e in self.experiments]
+        reaction_codes = dict(zip(S.index, range(1, len(S.index) + 1)))
+        metabolite_codes = dict(zip(S.columns, range(1, len(S.columns) + 1)))
+        experiment_codes = dict(zip(experiment_labels, range(1, len(experiment_labels) + 1)))
+        known_real_codes = dict(zip(known_reals.index, range(1, len(known_reals.index) + 1)))
+        kinetic_parameter_codes = dict(zip(kinetic_parameters['label'],
+                                           range(1, len(kinetic_parameters) + 1)))
+        thermodynamic_parameter_codes = dict(zip(thermodynamic_parameters['label'],
+                                                 range(1, len(kinetic_parameters) + 1)))
+        return {'reaction': reaction_codes,
+                'metabolite': metabolite_codes,
+                'experiment': experiment_codes,
+                'known_real': known_real_codes,
+                'kinetic_parameter': kinetic_parameter_codes,
+                'thermodynamic_parameter': thermodynamic_parameter_codes}
 
     def get_known_reals(self):
         out = {}
@@ -66,21 +95,17 @@ class EnzymeKatData():
         return pd.DataFrame.from_dict(out).assign(stan_code=lambda df: range(1, len(df) + 1))
 
     def get_parameters(self):
-        return (
+        parameters = (
             pd.io.json.json_normalize(self.reactions,
                                       'parameters',
                                       meta='name')
-            .assign(stan_code=lambda df: range(1, len(df) + 1))
-            .rename(columns={'name': 'reaction',
-                             'label': 'parameter'})
+            .rename(columns={'name': 'reaction', 'label': 'parameter'})
+            .assign(label=lambda df: df['reaction'].str.cat(df['parameter'], sep='_'))
         )
+        kinetic_parameters = parameters.query("type == 'kinetic'").copy()
+        thermodynamic_parameters = parameters.query("type == 'thermodynamic'").copy()
+        return kinetic_parameters, thermodynamic_parameters
 
-    def get_metabolites(self):
-        return (
-            pd.DataFrame({'name': self.stoichiometry.columns})
-            .assign(stan_code=lambda df: range(1, len(df) + 1))
-        )
-            
 
 def from_toml(path):
     t = toml.load(path)
