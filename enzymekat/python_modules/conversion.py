db1a04f00919742a2e9d0562b11f0e884fcb11e4
Teddy Groves <tedgro@nnfcb-l0601.home>
2019-07-04 09:50:19 +0200

    Chore: delete some files

--- enzymekat/python_modules/conversion.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def sem_pct_to_lognormal_sigma(sem_pct, mean, n=3):
-    sem = sem_pct/100 * mean
-    s = sem * np.sqrt(n)
-    return (np.sqrt(np.log(1 + (s ** 2)/(mean ** 2))))
-    
--- enzymekat/python_modules/utils.py
+++ enzymekat/python_modules/utils.py
@@ -1,5 +1,6 @@
-import pandas as pd
+import numpy as np
 import os
+import pandas as pd
 
 
 def match_string_to_file(s: str, path: str):
@@ -7,3 +8,10 @@ def match_string_to_file(s: str, path: str):
         return open(path, 'r').read() == s
     else:
         return False
+
+
+def sem_pct_to_lognormal_sigma(sem_pct, mean, n=3):
+    sem = sem_pct/100 * mean
+    s = sem * np.sqrt(n)
+    return (np.sqrt(np.log(1 + (s ** 2)/(mean ** 2))))
+    
