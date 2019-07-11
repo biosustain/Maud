3e3a3c3a5c764ece9a13e70b04d70f930beacacd
Teddy Groves <tedgro@nnfcb-l0601.eduroam.wireless.dtu.dk>
2019-07-11 15:36:16 +0200

    Bug: fix default sampling behaviour

--- enzymekat/cli.py
+++ enzymekat/cli.py
@@ -3,6 +3,7 @@
 import click
 from cmdstanpy import summary
 from enzymekat import sampling
+import os
 
 SAMPLING_DEFAULTS = {
     'rel_tol': 1e-13,
@@ -15,7 +16,11 @@ SAMPLING_DEFAULTS = {
     'n_cores': 4,
     'steady_state_time': 200
 }
+RELATIVE_PATH_EXAMPLE = '../data/in/linear.toml'
 
+def get_example_path(relative_path_example):
+    here = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(here, relative_path_example)
 
 @click.group()
 @click.help_option("--help", "-h")
@@ -44,7 +49,7 @@ pass
 @click.option('--steady_state_time', default=SAMPLING_DEFAULTS['steady_state_time'],
               help="Number of time units to simulate ODEs for")
 @click.argument('data_path', type=click.Path(exists=True, dir_okay=False),
-                default='data/in/linear.toml')
+                default=get_example_path(RELATIVE_PATH_EXAMPLE))
 def sample(data_path, **kwargs):
     """Sample from the model defined by the data at data_path."""
     runset = sampling.sample(data_path, **kwargs)
