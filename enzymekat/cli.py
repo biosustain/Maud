de9cc69104ffa62f33b4ec87d9f66275bd0ed9e9
NicholasCowie <nicow@biosustain.dtu.dk>
2019-08-15 14:19:17 +0200

    Feat: updating cmdline tools to work with relative CV

--- enzymekat/cli.py
+++ enzymekat/cli.py
@@ -105,6 +105,60 @@ def sample_CV(data_path, **kwargs):
 
 
 @cli.command()
+@click.option('--rel_tol', default=SAMPLING_DEFAULTS['rel_tol'],
+              help="ODE solver's relative tolerance parameter")
+@click.option('--abs_tol', default=SAMPLING_DEFAULTS['abs_tol'],
+              help="ODE solver's absolute tolerance parameter")
+@click.option('--max_steps', default=SAMPLING_DEFAULTS['max_steps'],
+              help="ODE solver's maximum steps parameter")
+@click.option('--likelihood', default=SAMPLING_DEFAULTS['likelihood'],
+              help="Whether (1) or not (0) to run the model in likelihood mode")
+@click.option('--n_samples', default=SAMPLING_DEFAULTS['n_samples'],
+              help="Number of post-warmup posterior samples")
+@click.option('--n_warmup', default=SAMPLING_DEFAULTS['n_warmup'],
+              help="Number of warmup samples")
+@click.option('--n_chains', default=SAMPLING_DEFAULTS['n_chains'],
+              help="Number of MCMC chains")
+@click.option('--n_cores', default=SAMPLING_DEFAULTS['n_cores'],
+              help="Number of chains to run in parallel")
+@click.option('--steady_state_time', default=SAMPLING_DEFAULTS['steady_state_time'],
+              help="Number of time units to simulate ODEs for")
+@click.argument('data_path', type=click.Path(exists=True, dir_okay=False),
+                default=get_example_path(RELATIVE_PATH_EXAMPLE))
+def sample_relative(data_path, **kwargs):
+    """Sample from the model defined by the data at data_path."""
+    stanfit = sampling_relative.sample(data_path, **kwargs)
+    print(stanfit.summary())
+
+
+@cli.command()
+@click.option('--rel_tol', default=SAMPLING_DEFAULTS['rel_tol'],
+              help="ODE solver's relative tolerance parameter")
+@click.option('--abs_tol', default=SAMPLING_DEFAULTS['abs_tol'],
+              help="ODE solver's absolute tolerance parameter")
+@click.option('--max_steps', default=SAMPLING_DEFAULTS['max_steps'],
+              help="ODE solver's maximum steps parameter")
+@click.option('--likelihood', default=SAMPLING_DEFAULTS['likelihood'],
+              help="Whether (1) or not (0) to run the model in likelihood mode")
+@click.option('--n_samples', default=SAMPLING_DEFAULTS['n_samples'],
+              help="Number of post-warmup posterior samples")
+@click.option('--n_warmup', default=SAMPLING_DEFAULTS['n_warmup'],
+              help="Number of warmup samples")
+@click.option('--n_chains', default=SAMPLING_DEFAULTS['n_chains'],
+              help="Number of MCMC chains")
+@click.option('--n_cores', default=SAMPLING_DEFAULTS['n_cores'],
+              help="Number of chains to run in parallel")
+@click.option('--steady_state_time', default=SAMPLING_DEFAULTS['steady_state_time'],
+              help="Number of time units to simulate ODEs for")
+@click.argument('data_path', type=click.Path(exists=True, dir_okay=False),
+                default=get_example_path(RELATIVE_PATH_EXAMPLE))
+def sample_relative_CV(data_path, **kwargs):
+    """Sample from the model defined by the data at data_path."""
+    stanfit = sampling_relative_CV.sample(data_path, **kwargs)
+    print(stanfit.summary())
+
+
+@cli.command()
 @click.argument('model_name')
 @click.argument('model_type',
               default='inference')
