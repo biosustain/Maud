97b21ddf2d63f56bafd3edb50143774768b0cf45
Teddy Groves <groves.teddy@gmail.com>
2019-10-08 14:40:03 +0200

    Test: test case for get_modular_rate_codes

--- tests/test_unit/test_code_generation.py
+++ tests/test_unit/test_code_generation.py
@@ -27,3 +27,19 @@ def test_create_stan_program():
             generated_stan_code.strip().splitlines(1),
         )
         assert len(list(diff)) == 0
+
+
+def test_get_modular_rate_codes():
+    """Check that the function get_modular_rate_codes works as expected."""
+    rxn_id = "r"
+    substrate_info = [["m1", -1], ["m2", -1]]
+    product_info = [["m3", 1], ["m4", 1]]
+    par_codes = {"r_keq": 0.1, "r_Ka": 0.1, "r_Kb": 0.3, "r_Kp": 0.2, "r_Kq": 0.2}
+    met_codes = {"m1": 1, "m2": 2, "m3": 3, "m4": 4}
+    expected_output = [[[1, 0.1, -1], [2, 0.3, -1]], [[3, 0.2, 1], [4, 0.2, 1]]]
+    assert (
+        code_generation.get_modular_rate_codes(
+            rxn_id, substrate_info, product_info, par_codes, met_codes
+        )
+        == expected_output
+    )
