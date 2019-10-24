21452a1e3a2428987780cf7b667adb68a1709155
Teddy Groves <groves.teddy@gmail.com>
2019-10-24 13:05:09 +0200

    Test: update test

--- tests/test_unit/test_io.py
+++ tests/test_unit/test_io.py
@@ -13,7 +13,7 @@ def test_load_maud_input_from_toml():
     """Test that the function load_maud_input_from_toml behaves as expected."""
     mi = io.load_maud_input_from_toml(os.path.join(data_path, "linear.toml"))
     assert mi.kinetic_model.reactions["r1"].stoichiometry == {"M1_ex": -1, "M1": 1}
-    assert mi.priors["r1_Keq"].target_id == "Keq"
+    assert mi.priors["r1_delta_g"].target_id == "r1"
     assert (
         mi.experiments["condition_1"].measurements["metabolite"]["M1"].target_type
         == "metabolite"
