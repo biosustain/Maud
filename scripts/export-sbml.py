07ceff94afcdce188e0902b9499a5c566b1bbd9d
NicholasCowie <nicow@biosustain.dtu.dk>
2021-02-15 15:04:16 +0100

    feat: constructing yaml format of odes

--- scripts/export-sbml.py
+++ scripts/export-sbml.py
@@ -1,6 +1,8 @@
 from maud import io, sampling
+from maud.sampling import get_full_stoichiometry
 from jinja2 import Environment, PackageLoader, Template
 import pandas as pd
+import numpy as np
 import os
 
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -40,79 +42,129 @@ odes:
 {%- for ode in odes %}
     - stateId: {{ode[0]}}
       rightHandSide: {{ode[1]}}
-      initialValue: 1
+      initialValue: {{ode[2]}}
     {% endfor -%}
 
 """)
 
-mi = io.load_maud_input_from_toml(os.path.join(HERE, "../tests/data/example/michaelis_menten"))
+mi = io.load_maud_input_from_toml(os.path.join(HERE, "../tests/data/example_features/michaelis_menten"))
+selected_experiment = None
+if selected_experiment == None:
+    selected_experiment = list(mi.stan_codes.experiment_codes.keys())[0]
+
 kinetic_model = mi.kinetic_model
 enzyme_codes = mi.stan_codes.enzyme_codes
 mic_codes = mi.stan_codes.mic_codes
-metabolite_codes = mi.stan_codes.metabolite_codes
+met_codes = mi.stan_codes.metabolite_codes
 drain_codes = mi.stan_codes.drain_codes
 reaction_codes = mi.stan_codes.reaction_codes
 _, _, S, _, _ = get_full_stoichiometry(kinetic_model,
                                        enzyme_codes,
-                                       metabolite_codes,
+                                       mic_codes,
                                        reaction_codes,
                                        drain_codes)
+prior_dfs = {
+        prior_type: pd.DataFrame(map(lambda p: p.__dict__, priors))
+        if len(priors) > 0
+        else pd.DataFrame([], columns=["location", "scale", "mic_code"])
+        for prior_type, priors in mi.priors.__dict__.items()
+    }
+formation_energies = prior_dfs["formation_energy_priors"] = (
+        prior_dfs["formation_energy_priors"]
+        .assign(stan_code=lambda df: df["metabolite_id"].map(met_codes))
+        .sort_values("stan_code")
+    )
 
 flux_vector = []
 
 for rxn in mi.kinetic_model.reactions.values():
 
-    tmp_stoich = pd.DataFrame(rxn.stoichiometry, index=[0]).transpose()
-    tmp_stoich = tmp_stoich.reset_index()
-    tmp_stoich.columns = ["mic_id", "stoichiometry"]
+    tmp_stoic = pd.DataFrame(rxn.stoichiometry, index=[0]).transpose()
+    tmp_stoic = tmp_stoic.reset_index()
+    tmp_stoic.columns = ["mic_id", "stoichiometry"]
+    tmp_formation_energies = {
+        mic: formation_energies[formation_energies["metabolite_id"] == mic[:-2]]["location"].values[0]
+        for mic in tmp_stoic["mic_id"].values
+        }
+    if rxn.water_stoichiometry:
+        tmp_stoic.loc[-1] = ["h2o", rxn.water_stoichiometry]
+        tmp_formation_energies.append({"h2o": -157.6})
 
-    tmp_Keq = 1
+    tmp_dG = 0
+    for _, row in tmp_stoic.iterrows():
+        tmp_dG += 0.008314*298.15*row["stoichiometry"]*tmp_formation_energies[row["mic_id"]]
+
+    tmp_Keq = np.exp(tmp_dG)
 
     for enz_id, enz in rxn.enzymes.items():
-        tmp_kms = {
-            prior.id: {
-                "mic_id": prior.mic_id,
-                "prior_mean": prior.location,
-            }
-            for prior_id, prior_entry in mi.priors.items() if prior_id == "kms"
-            for prior in prior_entry if prior.enzyme_id == enz_id
-        }
+        tmp_kms = prior_dfs["km_priors"]
+        tmp_kms = tmp_kms[tmp_kms["enzyme_id"] == enz_id]
+        tmp_kcat = prior_dfs["kcat_priors"]
+        tmp_kcat = tmp_kcat[tmp_kcat["enzyme_id"] == enz_id]
+        tmp_inh = prior_dfs["inhibition_constant_priors"]
+        if len(tmp_inh) > 0:
+            tmp_inh = tmp_inh[tmp_inh["enzyme_id"] == enz_id]
+        tmp_allo_act = prior_dfs["relaxed_dissociation_constant_priors"]
+        if len(tmp_allo_act) > 0:
+            tmp_allo_act = tmp_allo_act[tmp_allo_act["enzyme_id"] == enz_id]
+        tmp_allo_inh = prior_dfs["tense_dissociation_constant_priors"]
+        if len(tmp_allo_inh) > 0:
+            tmp_allo_inh = tmp_allo_inh[tmp_allo_inh["enzyme_id"] == enz_id]
+        tmp_L = prior_dfs["transfer_constant_priors"]
+        if len(tmp_L) > 0:
+            tmp_L = tmp_L[tmp_L["enzyme_id"] == enz_id]
 
-        tmp_kcat = {
-            prior.id: {
-                "prior_mean": prior.location,
-            }
-            for prior_id, prior_entry in mi.priors.items() if prior_id == "kcats"
-            for prior in prior_entry if prior.enzyme_id == enz_id
-        }
-
-        tmp_kms_df = pd.DataFrame.from_dict(tmp_kms).transpose().reset_index()
-        tmp_kms_df.columns = ["parameter_id", "mic_id", "prior_mean"]
-
-        tmp_enz_entry = tmp_kms_df.merge(tmp_stoich, left_on=['mic_id'], right_on=['mic_id'])
+        tmp_enz_entry = tmp_kms.merge(tmp_stoic, left_on=['mic_id'], right_on=['mic_id'])
 
         substrate_information = tmp_enz_entry[tmp_enz_entry['stoichiometry'] < 0]
         product_information = tmp_enz_entry[tmp_enz_entry['stoichiometry'] > 0]
 
-        substrate_entry = zip(substrate_information['mic_id'], substrate_information['parameter_id'], substrate_information['stoichiometry'].abs())
-        metabolite_denomintor = zip(tmp_enz_entry['mic_id'], tmp_enz_entry['parameter_id'], tmp_enz_entry['stoichiometry'].abs())
-        product_entry = zip(product_information['mic_id'], product_information['parameter_id'], product_information['stoichiometry'].abs())
-        haldane_entry = zip(tmp_enz_entry['parameter_id'], tmp_enz_entry['stoichiometry'])
-        
+        substrate_entry = zip(substrate_information['mic_id'], substrate_information['id'], substrate_information['stoichiometry'].abs())
+        metabolite_denomintor = zip(tmp_enz_entry['mic_id'], tmp_enz_entry['id'], tmp_enz_entry['stoichiometry'].abs())
+        product_entry = zip(product_information['mic_id'], product_information['id'], product_information['stoichiometry'].abs())
+        haldane_entry = zip(tmp_enz_entry['id'], tmp_enz_entry['stoichiometry'])
 
-        
+        competitive_entry = []
+        allosteric_inhibitors = []
+        allosteric_activators = []
+        for mod in enz.modifiers["competitive_inhibitor"]:
+            competitive_entry.append([mod.mic_id, tmp_inh[tmp_inh["mic_id"] == mod.mic_id].id])
+        for mod in enz.modifiers["allosteric_activator"]:
+            allosteric_activators.append(mod.mic_id, tmp_allo_act[tmp_allo_act["mic_id"] == mod.mic_id].id)
+        for mod in enz.modifiers["allosteric_inhibitor"]:
+            allosteric_inhibitors.append(mod.mic_id, tmp_allo_inh[tmp_allo_inh["mic_id"] == mod.mic_id].id)
+
+        Drreg = Template_Drreg.render(met_array=competitive_entry)
         Trf = Template_T_met.render(met_array=substrate_entry)
         Trr = Template_T_met.render(met_array=product_entry)
-        Hal = Template_Haldane.render(Km_array=haldane_entry, Keq=1)
+        Hal = Template_Haldane.render(Km_array=haldane_entry, Keq=tmp_Keq)
         Tr  = Template_Tr.render(enz=enz_id,
-                                 kcat=list(tmp_kcat.keys())[0],
+                                 kcat=tmp_kcat["id"].values[0],
                                  Trf=Trf,
                                  Trr=Trr,
                                  Hal=Hal)
         Dr = Template_Dr.render(met_array=metabolite_denomintor)
+        if competitive_entry == []:
+            Drreg = "0"
+        Allo_Act = Template_Allo_Act_Inh.render(met_array=allosteric_activators)
+        Allo_Inh = Template_Allo_Act_Inh.render(met_array=allosteric_inhibitors)
+        if allosteric_activators == []:
+            Allo_Act = "1"
+        if allosteric_inhibitors == []:
+            Allo_Inh = "1"
+        if len(tmp_L) > 0:
+            Allo = Template_Allo.render(L0=tmp_L["id"].values[0],
+                                        Dr=Dr,
+                                        Drreg=Drreg,
+                                        Allo_Inh=Allo_Inh,
+                                        Allo_Act=Allo_Act,
+                                        Subunits=enz.subunits)
+        else:
+            Allo = "1"
         flux = Template_flux.render(Tr=Tr,
                                     Dr=Dr,
-                                    Drreg="0")
+                                    Drreg=Drreg,
+                                    Allo=Allo)
 
         flux_vector.append(flux)
 
@@ -133,14 +185,25 @@ for met_ix, met_vec in enumerate(S.T.values):
         tmp_met_ode = "Zero"
 
     system_odes.append(tmp_met_ode)
+par_input = []
+par_input = par_input + [[par.id, par.location] for par in mi.priors.kcat_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.km_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.inhibition_constant_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.tense_dissociation_constant_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.relaxed_dissociation_constant_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.transfer_constant_priors]
+par_input = par_input + [[par.id, par.location] for par in mi.priors.drain_priors]
+par_input = par_input + [[par.enzyme_id, par.location] for par in mi.priors.enzyme_concentration_priors if par.experiment_id == selected_experiment]
 
-ode_input = [[met, system_odes[ix]] for ix, met in enumerate(mic_codes.keys())]
-par_input = [[par.id, par.location] for prior in mi.priors.values()
-                                    for par in prior]
-par_input.append(['r1', 1])
-par_input.append(['r2', 1])
-par_input.append(['r3', 1])
-
+met_init = pd.DataFrame(0.01, index=mic_codes, columns=["init"])
+for par in mi.priors.unbalanced_metabolite_priors:
+    if par.experiment_id == selected_experiment:
+        met_init.loc[par.mic_id, "init"] = par.location
+for exp in mi.experiments.experiments:
+    if exp.id == selected_experiment:
+        for meas in exp.measurements["mic"].values():
+            met_init.loc[meas.target_id, "init"] = meas.value
+ode_input = [[met, system_odes[ix], met_init.loc[met, "init"]] for ix, met in enumerate(mic_codes.keys())]
 yaml_input = Template_yaml.render(parameters=par_input,
                                   odes=ode_input)
 with open("test.yaml", 'w') as file:
