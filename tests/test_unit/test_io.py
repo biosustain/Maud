7d0d80b2bc20b997b9befe78065b130007e5401e
Teddy Groves <groves.teddy@gmail.com>
2021-05-11 14:50:43 +0200

    Test: fix some tests
    unit tests working, test_integration/test_pipeline.py fails

--- tests/test_unit/test_io.py
+++ tests/test_unit/test_io.py
@@ -25,6 +25,6 @@ def test_load_maud_input_from_toml():
     mi = io.load_maud_input_from_toml(os.path.join(data_path, "linear"))
     r1 = [r for r in mi.kinetic_model.reactions if r.id == "r1"][0]
     assert r1.stoichiometry == {"M1_e": -1, "M1_c": 1}
-    assert "r1" in mi.priors.kcat_priors.location.reset_index()["enzyme_id"].values
+    assert "r1" in mi.priors.priors_kcat.location.reset_index()["enzyme_id"].values
     for key, expected_codes in expected_stan_coords.items():
         assert mi.stan_coords.__dict__[key] == expected_codes
