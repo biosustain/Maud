7c1838ddcd746f6f9997db7b841b68471b8897f8
Teddy Groves <groves.teddy@gmail.com>
2021-09-19 16:53:55 +0200

    Test: fix column names in case of empty `pct_params` df

--- src/maud/io.py
+++ src/maud/io.py
@@ -511,15 +511,12 @@ def extract_2d_prior(
     out = raw.loc[lambda df: df["parameter_type"] == parameter_name].set_index(
         parameter_name_to_id_cols[parameter_name]
     )
-    pct_params = (
-        out[["pct1", "pct99"]]
-        .apply(
-            lambda row: qfunc(row["pct1"], 0.01, row["pct99"], 0.99),
-            axis=1,
-            result_type="expand",
-        )
-        .rename(columns={0: "location", 1: "scale"})
+    pct_params = out[["pct1", "pct99"]].apply(
+        lambda row: qfunc(row["pct1"], 0.01, row["pct99"], 0.99),
+        axis=1,
+        result_type="expand",
     )
+    pct_params.columns = ["location", "scale"]
     pct_params["location"] = np.exp(pct_params["location"])
     out[["location", "scale"]] = out[["location", "scale"]].fillna(pct_params)
     location, scale = (
