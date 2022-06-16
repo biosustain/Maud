19c5a9ccd81b3d335998a9e24d16cef13e5a147b
Teddy Groves <groves.teddy@gmail.com>
2022-06-16 15:46:22 +0200

    Test: add unit test for methionine cycle ode

--- tests/test_unit/test_model_ode.py
+++ tests/test_unit/test_model_ode.py
@@ -3,6 +3,7 @@
 import os
 
 import cmdstanpy
+import pytest
 from numpy import isclose
 
 
@@ -22,39 +23,60 @@ SIM_CONFIG = {
     "output_dir": None,
 }
 
+test_cases = [
+    (
+        "example_ode",
+        [
+            "conc[1,1]",
+            "conc[1,2]",
+            "conc[1,3]",
+            "conc[1,4]",
+            "flux[1,1]",
+            "flux[1,4]",
+        ],
+        ["conc|A", "conc|B", "conc|C", "conc|D", "flux|r1", "flux|r4"],
+        [5.0, 0.323117, 3.02187, 0.5, 0.421816, 2.11674],
+    ),
+    (
+        "methionine",
+        [
+            "conc[1,5]",
+            "flux[3,1]",
+            "allostery[4,5]",
+        ],
+        [
+            "conc|amet_c|dataset1",
+            "flux|METAT|dataset6",
+            "allostery|GNMT1_GNMT|dataset9",
+        ],
+        [5.931050e-5, 0.00108749, 0.0623294],
+    ),
+]
 
-def test_model_ode():
+
+@pytest.mark.parametrize(
+    "folder,csv_ixs,meaningful_ixs,expected_values", test_cases
+)
+def test_model_ode(folder, csv_ixs, meaningful_ixs, expected_values):
     """Test that the function get_input_data behaves as expected."""
-    input_path = os.path.join(data_path, "example_ode")
+    input_path = os.path.join(data_path, folder)
     init_data = os.path.join(input_path, "inits.json")
     input_data = os.path.join(input_path, "input_data_train.json")
     SIM_CONFIG["inits"] = init_data
     model = cmdstanpy.CmdStanModel(stan_file=model_path)
-    remap = {
-        "conc[1,1]": "A",
-        "conc[1,2]": "B",
-        "conc[1,3]": "C",
-        "conc[1,4]": "D",
-        "flux[1,1]": "r1",
-        "flux[1,4]": "r4",
-    }
-    true_values = {
-        "A": 5.0,
-        "B": 0.323117,
-        "C": 3.02187,
-        "D": 0.5,
-        "r1": 0.421816,
-        "r4": 2.11674,
-    }
     sim_values = (
         model.sample(data=input_data, **SIM_CONFIG)
         .draws_pd()
-        .rename(columns=remap)
-        .T.loc[true_values.keys(), 0]
-        .to_dict()
+        .loc[0, csv_ixs]
+        .to_list()
     )
-    msg = f"\nTrue values:\n {true_values}\nSimulated values:\n {sim_values}"
-    for true_value, sim_value in zip(
-        true_values.values(), sim_values.values()
+    for meaningful_ix, expected_value, sim_value in zip(
+        meaningful_ixs, expected_values, sim_values
     ):
-        assert isclose(true_value, sim_value), msg
+        var = meaningful_ix.split("|")[0]
+        ix = " ".join(meaningful_ix.split("|")[1:])
+        msg = (
+            f"\nExpected value of {var} {ix}: {expected_value}"
+            f"\nSimulated value:\n {sim_value}"
+        )
+        assert isclose(expected_value, sim_value), msg
