b51f3b55f7bebbf6d37bb90cded509016a636abf
Teddy Groves <groves.teddy@gmail.com>
2023-02-01 15:25:37 +0100

    Feat: move data files into package
    This change moves example inputs from the `tests` folder to `src/maud/data`.
    
    This is nice because these inputs are accessible to anyone who installs Maud,
    addressing issue #395.
    
    The change also makes it possible to switch Maud's tests to using the importlib
    resources module rather than having to mess around with paths.
    
    For easier maintenance I have removed some of the example inputs. These should
    either be obsoleted by making the linear example more featureful, or else added
    to the maud_data repo.

--- tests/test_unit/test_model_ode.py
+++ tests/test_unit/test_model_ode.py
@@ -1,76 +1,88 @@
 """Unit tests for model ode function."""
 
-import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Tuple
 
 import cmdstanpy
+import importlib_resources
 import pytest
 from numpy import isclose
 
+from maud import stan
+from maud.data.example_inputs import example_ode
 
-here = os.path.dirname(__file__)
-data_path = os.path.join(here, "..", "data")
-model_path = os.path.join(here, "..", "..", "src", "maud", "stan", "model.stan")
 
-SIM_CONFIG = {
-    "chains": 1,
-    "fixed_param": True,
-    "iter_warmup": 0,
-    "iter_sampling": 1,
-    "show_progress": False,
-    "threads_per_chain": 1,
-    "output_dir": None,
-}
+@dataclass
+class ExpectedValue:
+    """An expected value."""
 
-test_cases = [
-    (
-        "example_ode",
-        {
-            "conc_train": {
-                "B": ((0, 0), 5.0),
-                "C": ((0, 1), 0.323117),
-                "D": ((0, 2), 3.02187),
-            },
-            "flux_train": {
-                "r1": ((0, 0), 0.421816),
-                "r4": ((0, 3), 2.11674),
-            },
-        },
-    ),
-    #  methionine input commented out until we find the right values
-    # (
-    #     "methionine",
-    #     {
-    #         "conc_train": {
-    #             "amet_c|dataset1": ((1, 5), 5.931050e-5)
-    #         },
-    #         "flux_train": {
-    #             "METAT|dataset6": ((3, 1), 0.00108749),
-    #         },
-    #         "allostery": {
-    #             "GNMT1_GNMT|dataset9": ((4, 5), 0.0623294)
-    #         }
-    #     },
-    # ),
+    name: str
+    ix: Tuple[int, int]
+    value: float
+
+
+@dataclass
+class TestCase:
+    """A test case."""
+
+    name: str
+    inits_path: Path
+    input_data_path: Path
+    expected_conc_train: List[ExpectedValue]
+    expected_flux_train: List[ExpectedValue]
+
+
+TEST_CASES = [
+    TestCase(
+        name="example_ode",
+        inits_path=importlib_resources.files(example_ode).joinpath(
+            "inits.json"
+        ),
+        input_data_path=importlib_resources.files(example_ode).joinpath(
+            "input_data_train.json"
+        ),
+        expected_conc_train=[
+            ExpectedValue("B", (0, 0), 5.0),
+            ExpectedValue("C", (0, 1), 0.323117),
+            ExpectedValue("D", (0, 2), 3.02187),
+        ],
+        expected_flux_train=[
+            ExpectedValue("r1", (0, 0), 0.421816),
+            ExpectedValue("r4", (0, 3), 2.11674),
+        ],
+    )
 ]
 
 
-@pytest.mark.parametrize("folder,expected_values", test_cases)
-def test_model_ode(folder, expected_values):
+@pytest.mark.parametrize("test_case", TEST_CASES)
+def test_model_ode(test_case):
     """Test that the function get_input_data behaves as expected."""
-    input_path = os.path.join(data_path, folder)
-    init_data = os.path.join(input_path, "inits.json")
-    input_data = os.path.join(input_path, "input_data_train.json")
-    SIM_CONFIG["inits"] = init_data
-    model = cmdstanpy.CmdStanModel(stan_file=model_path)
-    mcmc = model.sample(data=input_data, **SIM_CONFIG)
+    model = cmdstanpy.CmdStanModel(
+        stan_file=importlib_resources.files(stan).joinpath("model.stan")
+    )
+    mcmc = model.sample(
+        data=str(test_case.input_data_path),
+        inits=str(test_case.inits_path),
+        chains=1,
+        fixed_param=True,
+        iter_warmup=0,
+        iter_sampling=1,
+        show_progress=False,
+        threads_per_chain=1,
+        output_dir=None,
+    )
     msg = (
         "\nExpected value of {var} {ix}: {expected_value}"
         "\nSimulated value:\n {sim_value}"
     )
-    for var, expected in expected_values.items():
-        sim_values = mcmc.draws_xr(vars=var).sel(chain=1, draw=0)[var]
-        for _, (ix, expected_value) in expected.items():
-            sim_value = sim_values.values[ix]
-            assert isclose(expected_value, sim_value), msg.format(
-                var=var, ix=ix, expected_value=expected_value
+    for var in ["conc_train", "flux_train"]:
+        sim_values = mcmc.draws_xr(vars=[var]).sel(chain=1, draw=0)[var].values
+        for expected_value in getattr(test_case, "expected_" + var):
+            sim_value = sim_values[expected_value.ix]
+            assert isclose(expected_value.value, sim_value), msg.format(
+                var=var,
+                ix=expected_value.ix,
+                expected_value=expected_value.value,
+                sim_value=sim_value,
             )
